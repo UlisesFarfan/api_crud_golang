119fa2e2a0bb9e2d0a9e52db09868b078b6ffd57
Ulises Farfan <ulisesfarfan296@gmail.com>
2023-09-09 21:07:32 -0300

    add tests

--- controllers/user.controller/user.controller_test.go
+++ controllers/user.controller/user.controller_test.go
@@ -30,27 +30,41 @@ func TestCreate(t *testing.T) {
 }
 
 func TestRead(t *testing.T) {
-
 	users, err := user_repository.Read()
-
 	if err != nil {
 		t.Error("Something went worng.")
 		t.Fail()
 	}
-
 	if len(users) == 0 {
 		t.Error("The request not has retorned data.")
 		t.Fail()
 	} else {
 		t.Log("Road test was soccessfuly.")
 	}
-
 }
 
+// Remember change userId
 func TestUpdate(t *testing.T) {
+	userUpdate := m.User{
+		Name: "update",
+	}
+	_, err := user_repository.Update(userUpdate, "64fbad5c6cc5b74c03ce139d")
+	if err != nil {
+		t.Error("Something went worng.")
+		t.Fail()
+	} else {
+		t.Log("Update test was soccessfuly.")
+	}
 
 }
 
+// Remember change userId
 func TestDelete(t *testing.T) {
-
+	err := user_repository.Delete("64fbad5c6cc5b74c03ce139d")
+	if err != nil {
+		t.Error("Something went worng.")
+		t.Fail()
+	} else {
+		t.Log("Update test was soccessfuly.")
+	}
 }
