ffbfa232b6b11e48cdc3c76b68983b3fe86cd010
Ulises Farfan <ulisesfarfan296@gmail.com>
2023-09-15 18:26:54 -0300

    add: cors

--- main.go
+++ main.go
@@ -10,12 +10,28 @@ import (
 
 func main() {
 	godotenv.Load()
-
-	port := os.Getenv("PORT")
-
 	router := gin.Default()
-
+	router.Use(CORSMiddleware())
 	app := r.Router(router)
-
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	app.Run(port)
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
